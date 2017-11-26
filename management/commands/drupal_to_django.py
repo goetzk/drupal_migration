e79fa3289dcb2212b31c3211499771505519cfbb
Karl Goetz <karl@kgoetz.id.au>
2017-11-26 15:01:37 +1100

    Fix syntax errors in management script
    Now parses correctly - should have done that before the last commit.

--- management/commands/drupal_to_django.py
+++ management/commands/drupal_to_django.py
@@ -2,7 +2,7 @@
 
 from wagtail.wagtailcore.models import Page
 
-from blog.models import BlogPage
+from blog.models import LegacyBlogPage
 
 from drupal_migration.models import Node, NodeRevision
 from drupal_migration.models import FieldRevisionBody, FieldRevisionFieldTags
@@ -19,16 +19,16 @@ for node_revision in NodeRevision.objects.all():
 
   # Try and load an existing page to add a new revision, create one if there isn't an existing page
   try:
-    page = BlogPage.objects.get(drupal_id=node_revision.nid)
+    page = LegacyBlogPage.objects.get(drupal_id=node_revision.nid)
     print "page found in db"
-	except BlogPage.DoesNotExist as dne:
-    page = BlogPage()
-		print "Page not found in DB: instantiated"
+  except LegacyBlogPage.DoesNotExist as dne:
+    page = LegacyBlogPage()
+    print "Page not found in DB: instantiated"
   except Exception as ee:
     print "Unhandled Exception:"
     print ee
 
-	page.date = datetime.fromtimestamp(node_revision.timestamp)
+  page.date = datetime.fromtimestamp(node_revision.timestamp)
   page.title = node_revision.title
   page.body = FieldRevisionBody.objects.get(revision_id = node_revision.vid).body_value
   page.revision_log = node_revision.log
@@ -40,10 +40,10 @@ for node_revision in NodeRevision.objects.all():
     tag_name = TaxonomyTermData.objects.get(tid=current_tag.field_tags_tid).name
     page.tags.add(tag_name)
 
-	revision = page.save_revision(
-	# 		user=self.import_user,
-			submitted_for_moderation=False,
-	)
+  revision = page.save_revision(
+  #     user=self.import_user,
+      submitted_for_moderation=False,
+  )
 
   # Use instead of save
   root_page.add_child(instance=page)
