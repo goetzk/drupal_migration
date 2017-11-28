a2b8faa90360bc441ef1308a7e3d935aa14e8d79
Karl Goetz <karl@kgoetz.id.au>
2017-11-28 21:34:12 +1100

    Add management command boilerplate
    This should enable the script to run via manage.py

--- management/commands/drupal_to_django.py
+++ management/commands/drupal_to_django.py
@@ -1,4 +1,4 @@
-# TODO: management command boilerplate
+from django.core.management.base import BaseCommand, CommandError
 
 from wagtail.wagtailcore.models import Page
 
@@ -14,66 +14,72 @@ from datetime import datetime
 from django.utils import timezone
 import pytz
 
-# Where in the site do we insert nodes under
-try:
-  root_page = Page.objects.get(slug='weblog')
-except Page.DoesNotExist as pdne:
-  print 'Are you sure you created your root page?'
 
-# TODO: if needed, hard code OUT publishing, I will review + publish by hand
-for node_revision in NodeRevision.objects.all():
-  print 'Using revision {0} from node {1}'.format(node_revision.vid, node_revision.nid)
+class Command(BaseCommand):
+	help = 'Migrates pages from Drupal to Django'
 
-  current_timestamp = datetime.fromtimestamp(node_revision.timestamp)
-  localised = pytz.timezone("UTC").localize(current_timestamp)
+	def handle(self, *args, **options):
 
-  # Try and load an existing page to add a new revision, create one if there isn't an existing page
-  try:
-    page = LegacyBlogPage.objects.get(drupal_id=node_revision.nid)
-    # print "page found in db"
-  except LegacyBlogPage.DoesNotExist as dne:
-    page = LegacyBlogPage()
-    # print "Page not found in DB: instantiated"
-  except Exception as ee:
-    print "Unhandled Exception:"
-    print ee
+		# Where in the site do we insert nodes under
+		try:
+			root_page = Page.objects.get(slug='weblog')
+		except Page.DoesNotExist as pdne:
+			self.stdout.write('Are you sure you created your root page?')
 
-  if page.date == localised:
-    # print 'already done this page/revision/timestamp'
-    continue
+		for node_revision in NodeRevision.objects.all():
+			self.stdout.write('Using revision {0} from node {1}'.format(node_revision.vid, node_revision.nid))
 
-  page_content = FieldRevisionBody.objects.get(revision_id = node_revision.vid).body_value
+			current_timestamp = datetime.fromtimestamp(node_revision.timestamp)
+			localised = pytz.timezone("UTC").localize(current_timestamp)
 
-  # print 'about to add stuff to page'
-  # I can fix broken previews (probably all of them ... Later)
-  # Or just have smaller number of words, also easy option and possibly nicer to look at?
-  page.preview = u'{0} ...'.format(page_content[:495])
-  page.body = page_content
-  page.date = localised
-  page.title = node_revision.title
-  page.revision_log = node_revision.log
-  page.drupal_id = node_revision.nid
+			# Try and load an existing page to add a new revision, create one if there isn't an existing page
+			try:
+				page = LegacyBlogPage.objects.get(drupal_id=node_revision.nid)
+				# self.stdout.write("page found in db"))
+			except LegacyBlogPage.DoesNotExist as dne:
+				page = LegacyBlogPage()
+				# self.stdout.write("Page not found in DB: instantiated")
+			except Exception as ee:
+				self.stdout.write("Unhandled Exception:")
+				self.stdout.write(ee)
 
-  # print 'create tags'
-  # Delete existing tags in case some are removed in this revision (eg Draft)
-  page.tags.clear()
-  # Establish page tags at this revision
-  for current_tag in FieldRevisionFieldTags.objects.filter(revision_id = node_revision.vid):
-    try:
-      tag_name = TaxonomyTermData.objects.get(tid=current_tag.field_tags_tid).name
-    except TaxonomyTermData.DoesNotExist as ttddne:
-      # print FieldRevisionFieldTags.objects.filter(revision_id = node_revision.vid).values()
-      break
+			if page.date == localised:
+				# self.stdout.write('already done this page/revision/timestamp')
+				continue
 
-    page.tags.add(tag_name)
+			page_content = FieldRevisionBody.objects.get(revision_id = node_revision.vid).body_value
 
-  # Use instead of save
-  if not page.id:
-    root_page.add_child(instance=page)
+			# self.stdout.write('about to add stuff to page')
+			# I can fix broken previews (probably all of them ... Later)
+			# Or just have smaller number of words, also easy option and possibly nicer to look at?
+			page.preview = u'{0} ...'.format(page_content[:495])
+			page.body = page_content
+			page.date = localised
+			page.title = node_revision.title
+			page.revision_log = node_revision.log
+			page.drupal_id = node_revision.nid
 
-  # print 'about to revision'
-  revision = page.save_revision(
-      submitted_for_moderation=False,
-  )
-  # print 'revision done'
+			# self.stdout.write('create tags')
+			# Delete existing tags in case some are removed in this revision (eg Draft)
+			page.tags.clear()
+			# Establish page tags at this revision
+			for current_tag in FieldRevisionFieldTags.objects.filter(revision_id = node_revision.vid):
+				try:
+					tag_name = TaxonomyTermData.objects.get(tid=current_tag.field_tags_tid).name
+				except TaxonomyTermData.DoesNotExist as ttddne:
+					# self.stdout.write(FieldRevisionFieldTags.objects.filter(revision_id = node_revision.vid).values())
+					break
+
+				page.tags.add(tag_name)
+				page.live = False
+
+			# Use instead of save
+			if not page.id:
+				root_page.add_child(instance=page)
+
+			# self.stdout.write('About to save revision')
+			revision = page.save_revision(
+					submitted_for_moderation=False,
+			)
+			# self.stdout.write('revision done')
 
